b2e2976673810980e4c1753d765bc24ef508ec7a
TMI_Yk <uncleteyvat@proton.me>
2024-08-13 00:25:16 -0700

    awg result fft print

--- PyCharmCode/AWG/byte_reader.py
+++ PyCharmCode/AWG/byte_reader.py
@@ -14,14 +14,18 @@ f_center = 7.75 # GHz
 f_chirp = 500 # MHz
 
 print(num_array.size)
+# plot every other number, since the data is I,Q,I,Q,I,Q,I,Q... interleaved and we only have Q-channel (why)
+I_data = num_array[0::2]
+Q_data = num_array[1::2]
+
 # offset = int(len(num_array)/2)
-offset = int(num_array.size * 10 / 11 - plot_length)
+offset = int(Q_data.size * 10 / 11 - plot_length/2)
 print(str(offset/2 * (1 / f_sample) * 1e6) + 'us')
 
-# plot every other number, since the data is I,Q,I,Q,I,Q,I,Q... interleaved and we only have I-channel
-a1 = num_array[1:plot_length:2]
+
+a1 = Q_data[0:int(plot_length/2)]
 a2 = np.zeros(50)
-a3 = num_array[(offset + 1):(offset + plot_length):2]
+a3 = Q_data[(offset):(offset + int(plot_length/2))]
 
 to_plot = np.concatenate([a1, a2, a3])
 print(to_plot)
@@ -29,4 +33,20 @@ print(to_plot)
 plt.plot(to_plot, color='b')
 plt.title('%.2fGHz center, %.2fMHz frequency sweep Linear FM AWG\n first %d sample vs last %d sample' % (
 f_center, f_chirp, plot_length / 2, plot_length / 2))
+plt.show()
+
+FFT_result = np.fft.fft(Q_data)/len(Q_data)
+N_decimate = 1000
+N_cut = 4
+N_stop = int(np.floor(len(FFT_result)/N_cut))
+print(N_stop)
+plot_result = FFT_result[:N_stop:N_decimate]
+# plot_result = num_array[0:N_stop:N_decimate]
+# plot_result = num_array
+print(plot_result[0:1000:100])
+fig, ax = plt.subplots()
+ax.plot(np.linspace(0,f_sample/N_cut,len(plot_result))/1E9,20*np.log10(np.abs(plot_result)))
+# ax.plot(np.linspace(0,f_sample/N_cut,len(plot_result))/1E9,((plot_result)))
+ax.set_xlabel('GHz')
+plt.title('FFT Results of first 1/%d' % N_cut)
 plt.show()
